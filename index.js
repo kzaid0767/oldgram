245c8f3e9d72e045f9bf88c782ddfe2416111c88
Kassim Zaid <kzaid0767@gmail.com>
2022-10-17 14:55:51 -0400

    looping workin

--- index.js
+++ index.js
@@ -28,3 +28,39 @@ const posts = [
     }
 ]
 
+const subContainer = document.getElementById('sub-container')
+
+function addLikes(post){
+    alert(post)
+}
+
+let innerStuff = ``
+function renderPosts(){
+    for(let el of posts){
+        innerStuff += `
+        <section class="top-section">
+        <img class="avatar" src="${el.avatar}" alt="avatar of van Gogh">
+        <div class="name">
+            <h3 class="font700 text-height name-text">${el.name}</h3>
+            <p class="text-height name-location">${el.location}</p>
+        </div>
+        </section>
+        <section class="middle-section">
+        <img class="image-post" src="${el.post}" alt="selfie of van Gogh">
+        </section>
+        <section class="bottom-section">
+        <div class="icons-container">
+            <img onclick='addLikes(${el})' class="icons" src="images/icon-heart.png" alt="hear icon">
+            <img class="icons" src="images/icon-comment.png" alt="comment icon">
+            <img class="icons" src="images/icon-dm.png" alt="dm icon">
+        </div>
+        <p class="likes font700 text-height">${el} likes</p>
+        <p class="comment text-height"><name class="font700">${el.username}</name> ${el.comment}</p>
+        </section>
+        `
+    }
+    
+    subContainer.innerHTML = innerStuff
+}
+
+renderPosts()
