897fc5a27bf4db2b2b8c20053c0bf0ffd1c4f4be
Kassim Zaid <81573513+kzaid0767@users.noreply.github.com>
2024-09-28 15:36:32 -0400

    project done, added hover, and increase like count

--- index.js
+++ index.js
@@ -1,33 +1,36 @@
 let posts = [
     {
-        id:0,
+        id:1,
         name: "Vincent van Gogh",
         username: "vincey1853",
         location: "Zundert, Netherlands",
         avatar: "images/avatar-vangogh.jpg",
         post: "images/post-vangogh.jpg",
         comment: "just took a few mushrooms lol",
-        likes: 21
+        likes: 21,
+        isLiked: false
     },
     {
-        id:1,
+        id:2,
         name: "Gustave Courbet",
         username: "gus1819",
         location: "Ornans, France",
         avatar: "images/avatar-courbet.jpg",
         post: "images/post-courbet.jpg",
         comment: "i'm feelin a bit stressed tbh",
-        likes: 4
+        likes: 4,
+        isLiked: false
     },
         {
-        id:2,
+        id:3,
         name: "Joseph Ducreux",
         username: "jd1735",
         location: "Paris, France",
         avatar: "images/avatar-ducreux.jpg",
         post: "images/post-ducreux.jpg",
         comment: "gm friends! which coin are YOU stacking up today?? post below and WAGMI!",
-        likes: 152
+        likes: 152,
+        isLiked: false
     }
 ]
 
@@ -38,13 +41,21 @@ document.addEventListener("click",handleLike)
 
 function handleLike(e){
     let num = Number(e.target.id)
-    // find object
+    console.log(num)
+    if(num){
+        // find object
     let obj = posts.find(item => item.id === num)
     
     //updating found object
-    obj.likes +=1
-    posts[num] = obj
+    if(!obj.isLiked){
+        obj.likes +=1
+    }else{
+        obj.likes -=1
+    }
+    obj.isLiked = !obj.isLiked
+    posts[num-1] = obj
     renderPosts(posts)
+    }
 }
 
 
@@ -65,8 +76,7 @@ function renderPosts(arr){
                 </div>
                 <div class="bottom-post">
                     <div class="icons-containter">
-                        <img class="icon" src="./images/icon-heart.png" 
-                            alt="like comment logo" id="${item.id}">
+                        <img class="icon" src="./images/icon-heart.png" alt="like comment logo" id="${item.id}">
                         <img class="icon" src="./images/icon-comment.png" alt="comment on post logo">
                         <img class="icon" src="./images/icon-dm.png" alt="send dm logo">
                     </div>
